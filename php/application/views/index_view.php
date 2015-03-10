18f11bf9579e24f000c1a68713ed42ba793ee901
Jason Tordsen <jason@jasons-mbp.home>
2015-03-10 12:09:11 -0400

    cfm icons

--- php/application/views/index_view.php
+++ php/application/views/index_view.php
@@ -11,25 +11,15 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 
         <!-- icons -->
-        <link rel="apple-touch-icon" sizes="57x57" href="<?= base_url(); ?>icons/apple-touch-icon-57x57.png">
-        <link rel="apple-touch-icon" sizes="60x60" href="<?= base_url(); ?>icons/apple-touch-icon-60x60.png">
-        <link rel="apple-touch-icon" sizes="72x72" href="<?= base_url(); ?>icons/apple-touch-icon-72x72.png">
-        <link rel="apple-touch-icon" sizes="76x76" href="<?= base_url(); ?>icons/apple-touch-icon-76x76.png">
-        <link rel="apple-touch-icon" sizes="114x114" href="<?= base_url(); ?>icons/apple-touch-icon-114x114.png">
-        <link rel="apple-touch-icon" sizes="120x120" href="<?= base_url(); ?>icons/apple-touch-icon-120x120.png">
-        <link rel="apple-touch-icon" sizes="144x144" href="<?= base_url(); ?>icons/apple-touch-icon-144x144.png">
-        <link rel="apple-touch-icon" sizes="152x152" href="<?= base_url(); ?>icons/apple-touch-icon-152x152.png">
-        <link rel="apple-touch-icon" sizes="180x180" href="<?= base_url(); ?>icons/apple-touch-icon-180x180.png">
-        <link rel="icon" type="image/png" href="<?= base_url(); ?>icons/favicon-32x32.png" sizes="32x32">
-        <link rel="icon" type="image/png" href="<?= base_url(); ?>icons/android-chrome-192x192.png" sizes="192x192">
-        <link rel="icon" type="image/png" href="<?= base_url(); ?>icons/favicon-96x96.png" sizes="96x96">
-        <link rel="icon" type="image/png" href="<?= base_url(); ?>icons/favicon-16x16.png" sizes="16x16">
-        <link rel="manifest" href="<?= base_url(); ?>icons/manifest.json">
-        <link rel="shortcut icon" href="<?= base_url(); ?>icons/favicon.ico">
-        <meta name="msapplication-TileColor" content="#ffffff">
-        <meta name="msapplication-TileImage" content="<?= base_url(); ?>icons/mstile-144x144.png">
-        <meta name="msapplication-config" content="<?= base_url(); ?>icons/browserconfig.xml">
-        <meta name="theme-color" content="#ffffff">
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="apple-touch-icon" sizes="57x57" href="/apple-touch-icon-57x57.png" />
+        <link rel="apple-touch-icon" sizes="72x72" href="/apple-touch-icon-72x72.png" />
+        <link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon-76x76.png" />
+        <link rel="apple-touch-icon" sizes="114x114" href="/apple-touch-icon-114x114.png" />
+        <link rel="apple-touch-icon" sizes="120x120" href="/apple-touch-icon-120x120.png" />
+        <link rel="apple-touch-icon" sizes="144x144" href="/apple-touch-icon-144x144.png" />
+        <link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon-152x152.png" />
         <!-- end icons -->
 
         <link rel="stylesheet" href="<?= base_url(); ?>css/main.css">
