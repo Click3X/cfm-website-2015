e49ee93baa76cc42913ebd726e22789cdd8d38db
Jason Tordsen <jason@Jasons-MacBook-Pro.local>
2015-03-12 15:44:13 -0400

    custom video tag

--- php/application/views/asset/banner-video_asset_view.php
+++ php/application/views/asset/banner-video_asset_view.php
@@ -1,6 +1,6 @@
 <div id="<?php echo $data->slug; ?>-video" class="project-asset-description">
 	<div class="project-asset-description-inner">
-		<h2><?php echo $asset->title; ?></h2>
+		<h2><?php echo $asset->heading; ?></h2>
 	</div>
 </div>
 <div id="<?php echo rand(); ?>-video" class="cfm-videoplayer project-asset-video" data-video="http://media.click3x.com/video/<?php echo $asset->media[0]->filename; ?>" data-poster="<?php echo base_url().'img/assets/video_posters/'.$asset->media[0]->filename; ?>.jpg">
@@ -8,7 +8,7 @@
         <video class="cfm-videoplayer-mobile" width="960" height="540" controls></video>
         <div class="cfm-videoplayer-poster">
             <div class="cfm-videoplayer-poster-header"><div class="cfm-videoplayer-poster-header-inner">
-              <div class="cfm-videoplayer-playbutton"><span class="label">VIDEO CASE STUDY</span><span class="arrow"></span></div>
+              <div class="cfm-videoplayer-playbutton"><span class="label"><?php echo !empty($asset->title) ? $asset->title : "Watch Case Study"; ?></span><span class="arrow"></span></div>
             </div></div>
         </div>
         <video class="cfm-videoplayer-desktop" width="960" height="540"></video>
