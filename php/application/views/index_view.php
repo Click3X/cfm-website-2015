b3e76c143942c7202ff6ca3927a3da941c479832
Jason Tordsen <jason@jasons-mbp.home>
2015-03-13 03:15:40 -0400

    favicon fix

--- php/application/views/index_view.php
+++ php/application/views/index_view.php
@@ -11,15 +11,15 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 
         <!-- icons -->
-        <link rel="shortcut icon" href="<?= base_url(); ?>favicon.ico" type="image/x-icon" />
-        <link rel="apple-touch-icon" href="<?= base_url(); ?>apple-touch-icon.png" />
-        <link rel="apple-touch-icon" sizes="57x57" href="<?= base_url(); ?>apple-touch-icon-57x57.png" />
-        <link rel="apple-touch-icon" sizes="72x72" href="<?= base_url(); ?>apple-touch-icon-72x72.png" />
-        <link rel="apple-touch-icon" sizes="76x76" href="<?= base_url(); ?>apple-touch-icon-76x76.png" />
-        <link rel="apple-touch-icon" sizes="114x114" href="<?= base_url(); ?>apple-touch-icon-114x114.png" />
-        <link rel="apple-touch-icon" sizes="120x120" href="<?= base_url(); ?>apple-touch-icon-120x120.png" />
-        <link rel="apple-touch-icon" sizes="144x144" href="<?= base_url(); ?>apple-touch-icon-144x144.png" />
-        <link rel="apple-touch-icon" sizes="152x152" href="<?= base_url(); ?>apple-touch-icon-152x152.png" />
+        <link rel="shortcut icon" href="<?= base_url(); ?>icons/favicon.ico" type="image/x-icon" />
+        <link rel="apple-touch-icon" href="<?= base_url(); ?>icons/apple-touch-icon.png" />
+        <link rel="apple-touch-icon" sizes="57x57" href="<?= base_url(); ?>icons/apple-touch-icon-57x57.png" />
+        <link rel="apple-touch-icon" sizes="72x72" href="<?= base_url(); ?>icons/apple-touch-icon-72x72.png" />
+        <link rel="apple-touch-icon" sizes="76x76" href="<?= base_url(); ?>icons/apple-touch-icon-76x76.png" />
+        <link rel="apple-touch-icon" sizes="114x114" href="<?= base_url(); ?>icons/apple-touch-icon-114x114.png" />
+        <link rel="apple-touch-icon" sizes="120x120" href="<?= base_url(); ?>icons/apple-touch-icon-120x120.png" />
+        <link rel="apple-touch-icon" sizes="144x144" href="<?= base_url(); ?>icons/apple-touch-icon-144x144.png" />
+        <link rel="apple-touch-icon" sizes="152x152" href="<?= base_url(); ?>icons/apple-touch-icon-152x152.png" />
         <!-- end icons -->
 
         <link rel="stylesheet" href="<?= base_url(); ?>css/main.css">
